ce6778e0207aba3dce32455c01a9c31cc923355e
Dieci1 <pasqualeioio@libero.it>
2024-04-10 17:23:15 +0200

    feat: added function to view movie trailers

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-  });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
+$(document).ready(function () {
     $('.carousel').carousel();
-  });
+});
+
+function toggleVideo() {
+    const trailer = document.querySelector('.trailer');
+    const video =document.querySelector('video');
+    video.pause();
+    trailer.classList.toggle('active');
+}
+
+function changeBg(bg, title) {
+    const banner = document.querySelector('.banner');
+    const contents = document.querySelectorAll('.content'); 
+    banner.style.background = `url("./images/img/movies/${bg}")`;
+    banner.style.backgroundSize = 'cover';
+    banner.style.backgroundPosition = 'center';
+
+    contents.forEach(content => {
+        content.classList.remove('active');
+        if (content.classList.contains(title)) {
+            content.classList.add('active');
+        }
+    });
+}
